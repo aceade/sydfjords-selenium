8b86278d65c865d2b6a64e3c41f45e8bbd509c0e
aceade@github.com <philiprowlands90@gmail.com>
2024-08-26 08:53:02 +0100

    #3 finally adding about page test

--- src/vue/test.js
+++ src/vue/test.js
@@ -1,5 +1,6 @@
-const {By, Builder} = require('selenium-webdriver');
+const {By, Builder, until} = require('selenium-webdriver');
 const assert = require("assert");
+const logging = require('selenium-webdriver/lib/logging');
 
   describe('OG Vue app', function () {
     let driver;
@@ -8,6 +9,9 @@ const assert = require("assert");
         // using Firefox because it closes the browser window when done
         // and Chrome reports some odd "policy violation" message
       driver = await new Builder().forBrowser('firefox').build();
+      const prefs = new logging.Preferences();
+      prefs.setLevel(logging.Type.BROWSER, logging.Level.DEBUG);
+
     });
 
     after(async () => await driver.quit());
@@ -62,6 +66,85 @@ const assert = require("assert");
 
       attractionsMenuVisible = await driver.findElement(By.id("attractionMenu")).isDisplayed();
       assert.equal(true, attractionsMenuVisible);
+    });
+
+    it("Loading About page", async() => {
+      await driver.get('https://aceade.github.io/sydfjords/#/about');
+      const headers = await driver.findElements(By.css("h2"));
+      const firstText = await headers[0].getText();
+      assert.equal(firstText, "Tourism offices");
+
+      const secondText = await headers[1].getText();
+      assert.equal(secondText, "Email us");
+
+      const thirdText = await headers[2].getText();
+      assert.equal(thirdText, "About");
+    });
+
+    it("Testing email form (invalid details)", async() => {
+      await driver.get('https://aceade.github.io/sydfjords/#/about');
+
+      let nameField = await driver.findElement(By.id("name"));
+      let emailField = await driver.findElement(By.id("email"));
+      let messageField = await driver.findElement(By.id("message"));
+      let submitButton = await driver.findElement(By.css("#emailForm button"));
+      let submitStatus = await driver.findElement(By.id("submitStatus"));
+
+      await nameField.sendKeys("Selena Ium");
+      await emailField.sendKeys("test@example.com");
+      await submitButton.click();
+
+      let statusText = await submitStatus.getText();
+      assert.equal(statusText, "Please fill out all fields");
+
+      await nameField.click();
+      await nameField.clear();
+      await nameField.sendKeys("Selena Ium");
+      await emailField.click();
+      await emailField.clear();
+      await emailField.sendKeys("test@example.com");
+      await messageField.click();
+      await messageField.clear();
+      await messageField.sendKeys("This is a test");
+      const nameText = await driver.findElement(By.id("name")).getAttribute("value");
+      const emailText = await emailField.getAttribute("value");
+      const messageText = await messageField.getAttribute("value");
+      console.info(`nameField text: ${nameText}\nemailText: ${emailText}\nmessageText: ${messageText}`);
+      await submitButton.click();
+
+      statusText = await submitStatus.getText();
+      assert.equal(statusText, "Sending...");
+      await driver.wait(until.elementTextIs(submitStatus, ""), 5000);
+    });
+
+    xit("Testing email form (valid details)", async() => {
+      await driver.get('https://aceade.github.io/sydfjords/#/about');
+
+      let nameField = await driver.findElement(By.id("name"));
+      let emailField = await driver.findElement(By.id("email"));
+      let messageField = await driver.findElement(By.id("message"));
+      let submitButton = await driver.findElement(By.css("#emailForm button"));
+      let submitStatus = await driver.findElement(By.id("submitStatus"));
+
+      await nameField.click();
+      await nameField.clear();
+      await nameField.sendKeys("Selena Ium");
+      await emailField.click();
+      await emailField.clear();
+      await emailField.sendKeys("test@example.com");
+      await messageField.click();
+      await messageField.clear();
+      await messageField.sendKeys("This is a test");
+      const nameText = await driver.findElement(By.id("name")).getAttribute("value");
+      const emailText = await emailField.getAttribute("value");
+      const messageText = await messageField.getAttribute("value");
+      console.info(`nameField text: ${nameText}\nemailText: ${emailText}\nmessageText: ${messageText}`);
+      await submitButton.click();
+
+      let statusText = await submitStatus.getText();
+      // since the email endpoint is no longer deployed, this will only be "sending"
+      assert.equal(statusText, "Sending...");
+      await driver.wait(until.elementTextIs(submitStatus, ""), 5000);
     });
   
     
